ce854d590c104d340872c50289bca29eb3427e95
MarkW <mark.emailtest@gmail.com>
2025-05-22 17:51:53 +0300

    next init

---
+++ src/app/layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./globals.css";
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
