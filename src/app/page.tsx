5826c0bda115e6d6bad9db2b4158365c06d80f86
MarkW <mark.emailtest@gmail.com>
2025-05-29 22:13:42 +0300

    static rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@ import StatusLabel, { Status } from "./components/status-label";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-3xl font-bold">Header</h1>
+      <h1 className="text-3xl font-bold">Header {new Date().toTimeString()}</h1>
       <StatusLabel status={Status.Active}>Active</StatusLabel>
       <StatusLabel status={Status.NotActive}>NotActive</StatusLabel>
       <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+      <StatusLabel status={Status.Suspended} disabled={true}>Suspended</StatusLabel>
     </main>
   );
 }
